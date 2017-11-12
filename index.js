fd0b380294a1790bf1cfa874095b69dce2013d06
Aharon Hyman <15872781+hymanaharon@users.noreply.github.com>
2017-11-13 00:01:36 +0200

    updated UI

--- index.js
+++ index.js
@@ -19,22 +19,28 @@ $("#minusClock").click(function(){        //timer goes down and stops at 1
       }
       console.log(sc)
 })
-$("#play").click(function() {
+$("#play").click(function() {                   //play onClick deactivate play +&- buttons
   $("#play").attr("disabled", true);
   $("#plusClock").attr("disabled", true);
   $("#minusClock").attr("disabled", true);
   console.log(sc)
-  var countSec = sc*60
+  var countSec = (sc*60)-1;
+  $("#sec").html(countSec%60);
+  $("#min").html(Math.floor(countSec/60));    //update sec onclick for smoother UI
   console.log(countSec);
   countDown = setInterval(function(){
     if (countSec>0){
       countSec -= 1
       console.log(countSec);
-      sc = countSec/60;
-    }
-    if (countSec%60 > 9) {
+      sc = countSec/60;                               //update sc but in minutes
+      if(countSec%60<10){
+        $("#sec").html("0"+countSec%60);
+      }else{
       $("#sec").html(countSec%60);
-      $("#min").html(Math.floor(countSec/60));
+      $("#min").html(Math.floor(countSec/60))};
+    }else if (countSec === 0) {
+      alert("Take A Break!")
+      clearInterval(countDown);
     }
   },1000)
   $("#pause").click(function () {
@@ -52,9 +58,30 @@ $("#play").click(function() {
     $("#plusClock").attr("disabled", false);
     $("#minusClock").attr("disabled", false);
   })
+
 })
 
-
+$("#short").click(function(){
+  $("#plusClock").attr("disabled", true);
+  $("#minusClock").attr("disabled", true);
+  $(".button1").addClass("button1a");
+  sc = 5;
+  $("#min").html(5);
+});
+$("#long").click(function(){
+  $("#plusClock").attr("disabled", true);
+  $("#minusClock").attr("disabled", true);
+  $(".button1").addClass("button1a");
+  sc = 10;
+  $("#min").html(10);
+});
+$("#pomodoro").click(function(){
+  $("#plusClock").attr("disabled", false);
+  $("#minusClock").attr("disabled", false);
+  $(".button1").removeClass("button1a");
+  sc = 25;
+  $("#min").html(25);
+});
 
 
 });
